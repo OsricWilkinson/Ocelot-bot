8cc2402ff30304c4bc6469d617a9c604f7b978b3
Osric Wilkinson <30068825+OsricWilkinson@users.noreply.github.com>
2019-05-07 13:15:19 +0100

    Add project files.

---
+++ EchoBot1/Models/ChatState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Models
+{
+    public class ChatState
+    {
+        public string LatestID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
