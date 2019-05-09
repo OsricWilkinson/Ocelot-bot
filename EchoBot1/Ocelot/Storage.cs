a2df0f839445f0eecab07a68de82243d9c4d415d
Osric Wilkinson <30068825+OsricWilkinson@users.noreply.github.com>
2019-05-09 11:35:55 +0100

    Still trying to get the path right

--- EchoBot1/Ocelot/Storage.cs
+++ EchoBot1/Ocelot/Storage.cs
@@ -8,7 +8,7 @@ namespace Ocelot
         public static Process LoadProcess()
         {
 
-            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\Resources\oct90001.json");
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\oct90001.json");
             return new Process(path);
         }
     }
